bf7f1af7b27e8ee139311eeab810f0e73ef64584
Gustavo <gustavosohne38@gmail.com>
2021-02-20 14:56:48 -0300

    finalmente

--- src/components/NewConversationModal.js
+++ src/components/NewConversationModal.js
@@ -1,9 +1,46 @@
-import React from 'react'
+/* eslint-disable no-unreachable */
+import React, { useRef, useState } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider'
 
 // import { Container } from './styles';
 
-function NewConversationModal() {
-  return <div />
-}
+export default function NewConversationModal() {
+  const { contacts } = useContacts()
 
-export default NewConversationModal
+  const [selectedContactId, setSelectedContactId] = useState([])
+
+  function handleCheckBoxChange(contactId) {
+    setSelectedContactId(prevSelectedContactId => {
+      
+    })
+  }
+
+  function handleSubmit() {
+    return alert('Entrou!')
+  }
+
+  return (
+    <>
+      <Modal.Header closeButton>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          {contacts.map((contact) => (
+            <>
+              <Form.Group controlId={contact.id} key={contact.id}>
+                <Form.Check type="checkBox"></Form.Check>
+                <Form.Control
+                  type="text"
+                  value={selectedContactId.includes(contact.id)}
+                  label={contact.name}
+                  onChange={() => handleCheckBoxChange(contact.id)}
+                />
+              </Form.Group>
+              <Button type="submit">Create</Button>
+            </>
+          ))}
+        </Form>
+      </Modal.Body>
+    </>
+  )
+}
